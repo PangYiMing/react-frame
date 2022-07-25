4f0683855da98a2dfbdfa86a70e96329ed75abb9
ympang <ympang@trip.com>
2022-07-25 11:10:26 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: pym
+ * @Date: 2022-01-04 11:51:03
+ * @LastEditors: pym
+ * @Description: 
+ * @LastEditTime: 2022-02-04 13:22:26
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
+
+function start() {
+  // fetch('/tripdocs/test/api', { method: 'POST' });
+}
+
+setTimeout(() => {
+  start();
+}, 1000);
+
+ReactDOM.render(<div className="app">qqq</div>, document.getElementById('root'));
